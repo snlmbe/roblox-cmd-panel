978a46700e5da7a2f3529dc8cff4255ea64e4f74
snlmbe <ssnlmbe@gmail.com>
2025-03-27 00:09:45 +0300

    Update tpgame.js

--- src/commands/tpgame.js
+++ src/commands/tpgame.js
@@ -1,10 +1,12 @@
 // tpgame.js
-window.commands = window.commands || {}
+window.commands = window.commands || {};
 
 window.commands.tpgame = {
     description: "Oyun ara (Örn: tpgame mm2)",
     run: function() {
-        const game = prompt("Oyun adı girin:")
-        if (game) window.location.href = `https://www.roblox.com/tr/discover/?Keyword=${game}`
+        const game = prompt("Oyun adı girin:");
+        if (game) {
+            window.location.href = `https://www.roblox.com/tr/discover/?Keyword=${game}`;
+        }
     }
-}
+};

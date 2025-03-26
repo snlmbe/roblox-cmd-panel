78d16f3de32b5ad27bcf68ee9632c2242ee67cee
snlmbe <ssnlmbe@gmail.com>
2025-03-27 00:10:19 +0300

    Update tpid.js

--- src/commands/tpid.js
+++ src/commands/tpid.js
@@ -1,1 +1,17 @@
+// tpid.js
+window.commands = window.commands || {};
+
+window.commands.tpid = {
+    description: "ID ile yönlen (Örn: tpid 142823291)",
+    run: function() {
+        const id = prompt("ID girin:");
+        if (id) {
+            if (id.length > 6) {
+                window.location.href = `https://www.roblox.com/games/${id}`;
+            } else {
+                window.location.href = `https://www.roblox.com/users/${id}/profile`;
+            }
+        }
+    }
+};
 

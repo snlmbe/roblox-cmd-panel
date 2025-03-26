2de43de4dcb9979a79350946181a7f33eeabd17b
snlmbe <ssnlmbe@gmail.com>
2025-03-27 00:11:07 +0300

    Update roll.js

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -1,1 +1,10 @@
+// roll.js
+window.commands = window.commands || {};
 
+window.commands.roll = {
+    description: "Zar at (1-1000)",
+    run: function() {
+        const roll = Math.floor(Math.random() * 1000) + 1;
+        alert(`🎲 Zar: ${roll}`);
+    }
+};

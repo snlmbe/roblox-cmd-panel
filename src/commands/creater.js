6802ddc3f3ab78ed3abc82fb16d96f4d79bc4190
snlmbe <ssnlmbe@gmail.com>
2025-03-27 00:10:40 +0300

    Update creater.js

--- src/commands/creater.js
+++ src/commands/creater.js
@@ -1,1 +1,10 @@
+// creater.js
+window.commands = window.commands || {};
+
+window.commands.creater = {
+    description: "Yapımcı profilini aç",
+    run: function() {
+        window.location.href = 'https://www.roblox.com/tr/users/1161867135/profile';
+    }
+};
 

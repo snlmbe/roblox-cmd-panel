df9b350c7554414e541483bb26583b1a17f486fa
snlmbe <ssnlmbe@gmail.com>
2025-03-27 00:10:02 +0300

    Update tpuser.js

--- src/commands/tpuser.js
+++ src/commands/tpuser.js
@@ -1,1 +1,12 @@
+// tpuser.js
+window.commands = window.commands || {};
 
+window.commands.tpuser = {
+    description: "Kullanıcı ara (Örn: tpuser bac)",
+    run: function() {
+        const user = prompt("Kullanıcı adı girin:");
+        if (user) {
+            window.location.href = `https://www.roblox.com/search/users?keyword=${user}`;
+        }
+    }
+};

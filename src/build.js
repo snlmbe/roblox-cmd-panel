1277c6d8c065e6d0d326acff3f9149e37e883fd0
snlmbe <ssnlmbe@gmail.com>
2025-03-26 23:47:33 +0300

    Update build.js

--- src/build.js
+++ src/build.js
@@ -1,1 +1,10 @@
+const esbuild = require('esbuild');
 
+esbuild.build({
+    entryPoints: ['src/main.user.js'],
+    bundle: true,
+    outfile: 'dist/main.user.js',
+    format: 'iife',
+    target: ['chrome58'],
+    loader: { '.css': 'text' },
+}).catch(() => process.exit(1));

7d17be7b62e2b187a3661b2168c5c134d116ab2a
snlmbe <ssnlmbe@gmail.com>
2025-03-26 23:45:12 +0300

    Update CommandHandler.js

--- src/core/CommandHandler.js
+++ src/core/CommandHandler.js
@@ -1,1 +1,15 @@
+export class CommandHandler {
+    constructor() {
+        this.commands = new Map();
+    }
 
+    register(command) {
+        this.commands.set(command.name, command);
+    }
+
+    async execute(commandName, args) {
+        const command = this.commands.get(commandName);
+        if (!command) throw new Error(`Command not found: ${commandName}`);
+        await command.execute(args);
+    }
+}

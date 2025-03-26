4a15a1d900464f93dd79b4d6358daf68ba0d0ed5
snlmbe <ssnlmbe@gmail.com>
2025-03-26 23:46:17 +0300

    Update tpgame.js

--- src/commands/tpgame.js
+++ src/commands/tpgame.js
@@ -1,1 +1,9 @@
-
+export default {
+    name: "tpgame",
+    description: "Oyun arama komutu",
+    
+    async execute(args) {
+        const gameName = args.join(' ');
+        window.location.href = `https://www.roblox.com/tr/discover/?Keyword=${gameName}`;
+    }
+}
